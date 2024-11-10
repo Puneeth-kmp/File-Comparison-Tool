e676da702dd054573e9423aadd7994fd8b3f9133
Puneeth-kmp <142174684+Puneeth-kmp@users.noreply.github.com>
2024-11-10 16:37:52 +0530

    Update disbin.py

--- disbin.py
+++ disbin.py
@@ -208,7 +208,7 @@ def main():
         if logo:
             st.markdown(
                 """
-                <div class="logo-container">
+                <div class="logo-container" style="text-align: center;">
                     <img src="data:image/png;base64,{}" style="width: 30%;">
                     <h1 class="header-text"></h1>
                 </div>
